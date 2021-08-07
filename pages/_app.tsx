aee7ef57503d3dd84b54e2d2f55f028f4e99ce18
Trey Moen <treymoen99@gmail.com>
2021-08-06 17:13:56 -0700

    Add meta data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,6 +23,19 @@ export default function MyApp(props: AppProps) {
       <Head>
         <title>My page</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <link rel="manifest" href="/manifest.json" />
+        <title>avery and trey</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="description" content="We're getting married!" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta property="og:title" content="avery and trey" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://owen2moen.com/" />
+        <meta property="og:description" content="We're getting married!" />
+        <meta property="og:image" content="/img/DSC_0399-min.jpg" />
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
