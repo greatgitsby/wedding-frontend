f6ee7595d132eac01dde4ae445aac8ab5af597fb
Trey Moen <treymoen99@gmail.com>
2021-08-06 22:12:25 -0700

    Updates to layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,8 @@ import { Button, Grid, ThemeProvider, Typography } from '@material-ui/core';
 import { Theme, createStyles, makeStyles, createTheme } from '@material-ui/core/styles';
 import { AuthFlowImageProps, getAuthFlowImageProps } from '../src/Images';
 
+import Image from "next/image";
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
@@ -55,31 +57,34 @@ export default function Index(props: AuthFlowImageProps) {
   const classes = useStyles();
   const images = props.images;
 
+  const numImageColumns = 4;
+  const pixelGap = 6;
+  const imageWidth = (900 - (pixelGap * numImageColumns)) / numImageColumns;
+
   shuffleArray(images);
 
   return (
     <Grid container justifyContent="center">
       <Grid item>
-        <Box sx={{ my: 12 }}>
+        <Box sx={{ my: 10 }}>
           <ThemeProvider theme={typographyTheme}>
             <Typography variant="h3" align="center">
-              avery + trey
+              trey + avery
             </Typography>
           </ThemeProvider>
         </Box>
 
         <Box sx={{ my: 4 }}>
           <ImageList
-            rowHeight={275}
+            rowHeight={200}
             className={classes.imageList}
-            cols={4}
+            gap={pixelGap}
+            cols={numImageColumns}
           >
             {images.map((item, i) => {
-              const cols = 2;
-
               return (
-                <ImageListItem key={i} cols={cols}>
-                  <img src={item} />
+                <ImageListItem key={i} cols={1} rows={2}>
+                  <Image src={item} height={450} width={imageWidth} layout="fixed" objectFit="cover" />
                 </ImageListItem>
               );
             })}
@@ -87,8 +92,8 @@ export default function Index(props: AuthFlowImageProps) {
         </Box>
         
         <Box sx={{ my: 4 }}>
-          <Button fullWidth variant="contained" color="primary" href="/">
-            Coming Soon
+          <Button fullWidth variant="contained" color="primary" href="/login">
+            View RSVP
           </Button>
         </Box>
       
