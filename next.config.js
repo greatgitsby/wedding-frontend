84a820f55e5c7625fab048b70a7c13d3a1b0b4d3
Trey Moen <treymoen99@gmail.com>
2021-08-07 23:56:24 -0700

    Add node loader

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   webpack: (config) => {
+    config.module.rules.push({
+      test: /\.node$/,
+      loader: "node-loader",
+    });
+    
     config.externals = config.externals || {};
     config.externals["sharp"] = "commonjs sharp"
     return config
