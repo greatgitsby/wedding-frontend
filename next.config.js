748a2c581c68003145166944f62382ce17666ff3
Trey Moen <treymoen99@gmail.com>
2021-08-07 20:26:08 -0700

    Update resources

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ module.exports = {
         config.externals = config.externals || {};
 
         config.externals["sharp"] = "commonjs sharp";
-        config.module.rules.push(
-            { test: /\.node$/, use: "node-loader"}
-        );
 
         return config;
     },
