07b082f8d348aa6d02539c2c9efa2f74ece0f4b7
Trey Moen <treymoen99@gmail.com>
2021-08-07 20:03:23 -0700

    Update webpack cfg

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 module.exports = {
     webpack5: true,
     webpack: (config) => {
-        if (!config.externals) {
-            config.externals = {};
-        }
+        config.externals = config.externals || {};
 
         config.externals["sharp"] = "commonjs sharp";
-        
+        config.module.rules.push(
+            { test: /\.node$/, use: "node-loader"}
+        );
+
         return config;
     },
 }
