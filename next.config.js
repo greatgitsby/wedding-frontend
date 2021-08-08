79c6c9be3eb4c4320a57e2d27785254fb9a2cf99
Trey Moen <treymoen99@gmail.com>
2021-08-07 19:50:54 -0700

    Update webpack config for sharp.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-    webpack5: true
+    webpack5: true,
+    webpack: (config) => {
+        if (!config.externals) {
+            config.externals = {};
+        }
+
+        config.externals["sharp"] = "commonjs sharp";
+        
+        return config;
+    },
 }
