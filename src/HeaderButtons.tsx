b142b27247d39f4541ab6a9624e6074a46e4d268
Trey Moen <treymoen99@gmail.com>
2021-12-01 21:57:20 -0800

    modularize header button configuration

--- src/HeaderButtons.tsx
+++ src/HeaderButtons.tsx
@@ -1,23 +1,35 @@
 import { Button, ButtonGroup } from '@mui/material';
 import { useRouter } from 'next/router';
 
+interface HeaderButtonConfig {
+  name: string;
+  path: string;
+};
+
 export default function HeaderButtons() {
-  const router = useRouter();
-  const path = router.pathname;
+  const path = useRouter().pathname;
+  const buttons: HeaderButtonConfig[] = [
+    {
+      name: "Gallery",
+      path: "/"
+    },
+    {
+      name: "Details",
+      path: "/details"
+    },
+    {
+      name: "Registry",
+      path: "/registry"
+    }
+  ];
 
   return (
-    <>
-      <ButtonGroup color="secondary" aria-label="" size="large">
-        <Button variant={path === "/" ? "contained" : "outlined"} href="/">
-          Gallery
+    <ButtonGroup color="secondary" aria-label="" size="large">
+      {buttons.map((button: HeaderButtonConfig) => (
+        <Button key={button.name} variant={path === button.path ? "contained" : "outlined"} href={button.path}>
+          {button.name}
         </Button>
-        <Button variant={path === "/details" ? "contained" : "outlined"} href="/details">
-          Details
-        </Button>
-        <Button variant={path === "/registry" ? "contained" : "outlined"} href="/registry">
-          Registry
-        </Button>
-      </ButtonGroup>
-    </>
+      ))}
+    </ButtonGroup>
   );
 }
